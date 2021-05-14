f7e12d8b82f55c42c0c4fcdfc775f575a216d1c1
Anand987 <anandsharma110397@gmail.com>
2021-05-14 10:21:44 +0530

    color filler completed

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "dodgerblue", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const rand = getRandomNumber();
+
+  document.body.style.backgroundColor = colors[rand];
+  color.textContent = colors[rand];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
