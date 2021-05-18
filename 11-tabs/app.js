018e8cf2e9f915f248f44dc22c1ac628675ebe35
Anand987 <anandsharma110397@gmail.com>
2021-05-18 11:42:54 +0530

    tab project completed

--- 11-tabs/app.js
+++ 11-tabs/app.js
@@ -1,19 +1,26 @@
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
+
 about.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
   if (id) {
-    // remove selected from other buttons
+    // remove active class from all/other btns
     btns.forEach(function (btn) {
       btn.classList.remove("active");
+      e.target.classList.add("active");
     });
-    e.target.classList.add("active");
-    // hide other articles
+
+    // hide all the other article
     articles.forEach(function (article) {
       article.classList.remove("active");
+      // if (id === article.getAttribute("id")) { // SOLUTION 1
+      //   article.classList.add("active");
+      // }
     });
-    const element = document.getElementById(id);
+
+    // showing selected content
+    const element = document.getElementById(id); // SOLUTION 2
     element.classList.add("active");
   }
 });
